5c5a09af45c4d58941797a0bf34389eaaba78a10
Dnilo00 <A01738467@tec.mx>
2025-01-11 00:36:30 -0600

    Daniel 1.1 Modelo educativo, versión base
    terminada

---
+++ sp/modelo_educativo.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+
+<link rel="stylesheet" href="../styles_sp/modelo_educativo.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+<script src="sp.js"></script>
+
+
+<body>
+    <section class="first_section_niveles_educativos">
+        <div class="first_section_niveles_educativos_img" style="background-image: url(../img_sp/modelo_educativo/portada.png);"></div>
+        <div class="first_section_niveles_educativos_portada">
+            <h2 class="first_section_niveles_educativos_title">Preparatoria Abierta ISAN</h2>
+            <p class="first_section_niveles_educativos_sibtitle"><strong>Formando estudiantes que desarrollan su potencial y su propio conocimiento.</strong></p>
+        </div>
+    </section>
+
+    <section class="second_section">
+        <div class="second_section_info">
+            <h2>Tu educación es nuestra prioridad</h2>
+            <p>En nuestro instituto, creemos que cada estudiante es el eje central del proceso de aprendizaje. Nuestro modelo educativo está <strong>diseñado para considerar las características individuales, fomentando el desarrollo académico, personal y profesional de cada alumno.</strong></p>
+            <p>Promovemos un enfoque práctico y constructivista, donde los estudiantes adquieren conocimientos aplicando su capacidad de análisis, indagación y comprensión en contextos reales. A través de la experimentación y el pensamiento crítico, nuestros alumnos desarrollan habilidades para entender el "cómo" y el "porqué" de los fenómenos que impactan su entorno, preparándose para destacar en las áreas de contaduría, derecho y más allá.</p>
+        </div>
+        <div class="second_section_img">
+            <img src="../img_sp/modelo_educativo/second_section.png">
+        </div>
+    </section>
+
+
+
+
+<section class="third_section">
+    <h2 class="third_section_title">ISAN</h2>
+    <div class="third_section_grid">
+
+        <div class="third_section_grid_escudo">
+            <img src="../img_sp/modelo_educativo/logo_isan.png" alt="Logo ISAN">
+        </div>
+
+        <div class="third_section_grid_video">
+            <h2>Contamos con Validez Oficial</h2>
+            <div class="third_section_grid_video_imgs">
+                <video controls poster="../img_sp/modelo_educativo/third_section_video_poster.png" class="third_section_grid_himno_video">
+                    <source src="../img_sp/modelo_educativo/third_section_video.mp4" type="video/mp4">
+                    Tu navegador no soporta la reproducción de video. Por favor, actualiza tu navegador.
+                </video>
+            </div>
+        </div>
+
+        <div class="third_section_servicios">
+            <h2>Conoce lo que ofrece ISAN para TI</h2>
+            <a href="javascript:void(0)">Preparatoria Abierta</a>
+            <a href="javascript:void(0)">Licenciaturas</a>
+            <a href="javascript:void(0)">Maestrías</a>
+        </div>
+    </div>
+</section>
+
+
+
+    <section class="fourth_section_atributos">
+        <div class="fourth_section_atributos_div">
+            <h2 id="fourth_section_atributos_h21">Atributos de nuestro método de enseñanza</h2>
+            <h2 id="fourth_section_atributos_h22">VS otros modelos educativos</h2>
+        </div>
+    </section>
+
+
+    <section class="fifth_section_metodos">
+        <div class="fifth_section_grid">
+            <div class="fifth_section_grid_first fifth_section_grid_first_hover">
+                <h3>Modelo Escolarizado</h3>
+                <div class="fifth_section_grid_first_info">
+                    <ul class="fifth_section_grid_first_info_ul">
+                        <li>  Interacción constante con docentes.</li>
+                        <li>  Acceso inmediato a recursos académicos.</li>
+                        <li>  Asistencia diaria, refuerza disciplina.</li>
+                        <li>  Aprendizaje estructurado y continuo.</li>
+                        <li>  Ideal para ritmo académico regular.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="fifth_section_grid_first fifth_section_grid_first_hover">
+                <h3>Modelo Mixta Sabatina</h3>
+                <div class="fifth_section_grid_first_info">
+                    <ul class="fifth_section_grid_first_info_ul">
+                        <li>  Clases sabatinas, ideal para quienes trabajan.</li>
+                        <li>  Combina presencialidad y autonomía.</li>
+                        <li>  Flexibilidad en el tiempo de estudio.</li>
+                        <li>  Enfoque práctico en sesiones presenciales.</li>
+                        <li>  Adaptado a necesidades personales.</li>
+
+                    </ul>
+                </div>
+            </div>
+
+            <div class="fifth_section_grid_first column_2">
+                <h3>Otros Modelos</h3>
+                <div class="fifth_section_grid_first_info">
+                    <ul class="fifth_section_grid_first_info_ul">
+                        <li>  Métodos poco dinámicos.</li>
+                        <li>  Poca estructura.</li>
+                        <li>  Riesgo de información incorrecta.</li>
+                        <li>  Sin evaluación constante.</li>
+                        <li>  Falta de supervisión directa.</li>
+                        <li>  Dependencia de memorización.</li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sixth_section">
+        <div class="sixth_section_grid">
+            <div class="sixth_section_flex_grid">
+                <img src="../img_sp/modelo_educativo/sixth_section.png" alt="Estudiantes de Bachillerato Internacional">
+            </div>
+            <div class="sixth_section_flex_info">
+                <h2 id="h21">Programas Académicos de</h2>
+                <h2 id="h22">ISAN</h2>
+                <h4 id="h41">Educación para cada etapa de tu vida académica</h4>
+                <p id="p1">En ISAN, nuestro compromiso es ofrecer una formación integral y de calidad, brindando las herramientas necesarias para que los estudiantes destaquen en cualquier ámbito profesional.</p>
+                <p id="p2">Ofrecemos programas desde la preparatoria abierta hasta maestrías en contaduría y derecho, adaptados a las necesidades de nuestros estudiantes y con validez oficial.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="seventh_section">
+        <div class="seventh_section_grid">
+            <div class="seventh_section_grid_1">
+                <h2>Preparatoria Abierta</h2>
+                <p>Este programa está diseñado para estudiantes que buscan flexibilidad en su educación y desean obtener su certificado de preparatoria a su propio ritmo.</p>
+                <p><strong>Objetivo:</strong> Preparar a los estudiantes con bases sólidas para ingresar a estudios superiores, desarrollando habilidades de investigación, análisis y autonomía en su aprendizaje.</p>
+                <a href="javascript:void(0)"><strong>More Information <i class="fa-solid fa-chevron-right"></i></strong></a>
+            </div>
+            <div class="seventh_section_grid_2">
+                <img src="../img_sp/modelo_educativo/seventh_section_preparatoria.png">
+            </div>
+
+            <div class="seventh_section_grid_1">
+                <h2>Licenciaturas en Contaduría y Derecho</h2>
+                <p><strong>Contaduría:</strong> Aprende a gestionar recursos financieros, desarrollar estrategias contables y analizar datos económicos esenciales para el éxito empresarial.</p>
+                <p><strong>Derecho:</strong> Adquiere los conocimientos legales necesarios para defender derechos, interpretar leyes y contribuir al sistema de justicia.</p>
+                <a href="javascript:void(0)"><strong>More Information <i class="fa-solid fa-chevron-right"></i></strong></a>
+            </div>
+            <div class="seventh_section_grid_2">
+                <img src="../img_sp/modelo_educativo/seventh_section_licenciatura.png">
+            </div>
+
+            <div class="seventh_section_grid_1">
+                <h2>Maestrías</h2>
+                <p>Nuestros programas de maestría están diseñados para profesionales que buscan especializarse y destacar en su campo.</p>
+                <p><strong>Objetivo:</strong> Ampliar el conocimiento y las habilidades específicas, garantizando un impacto positivo en el ámbito profesional y académico.</p>
+                <a id="a3" href="javascript:void(0)"><strong>More Information <i class="fa-solid fa-chevron-right"></i></strong></a>
+            </div>
+            <div class="seventh_section_grid_2">
+                <img src="../img_sp/modelo_educativo/seventh_section_maestria.png">
+            </div>
+        </div>
+    </section>
+
+    <section class="eighth_section">
+        <div class="eighth_section_grid">
+            <div class="eighth_section_grid_info">
+                <h2>Más de <strong>15 años</strong> siendo parte de</h2>
+                <h2><strong>las mejores escuelas en México</strong></h2>
+            </div>
+            <div class="eighth_section_grid_img">
+                <img src="../img_sp/modelo_educativo/logo_isan.png">
+            </div>
+        </div>
+        <div class="eighth_section_grid_2_img">
+            <ul class="timeline">
+                <li class="tenth_section" style="--accent-color:#003466">
+                    <div class="date">2007</div>
+                    <div class="title">Fundación del Colegio ISAN</div>
+                    <div class="descr">El Colegio ISAN abrió sus puertas en Torreón, Coahuila, ofreciendo un enfoque innovador en educación de preparatoria abierta. Desde el inicio, destacó por su enseñanza personalizada y su compromiso con la formación integral de los estudiantes.</div>
+                </li>
+                <li class="eleventh_section" style="--accent-color:#ffcb36">
+                    <div class="date">2012</div>
+                    <div class="title">Primera Generación de Egresados</div>
+                    <div class="descr">Cinco años después de su fundación, el Colegio ISAN celebró con orgullo la graduación de su primera generación de estudiantes de preparatoria abierta, consolidándose como una institución educativa de referencia en la región.</div>
+                </li>
+                <li class="twelfth_section" style="--accent-color:#db1219">
+                    <div class="date">2017</div>
+                    <div class="title">Apertura de Nuevas Carreras</div>
+                    <div class="descr">Con el objetivo de expandir su oferta educativa, el Colegio ISAN introdujo licenciaturas en Derecho y Contaduría, brindando a los estudiantes la oportunidad de obtener una formación profesional de alta calidad.</div>
+                </li>
+                <li class="thirteenth_section" style="--accent-color:#0e0b0e">
+                    <div class="date">2022</div>
+                    <div class="title">Transformación Digital</div>
+                    <div class="descr">El colegio dio un paso hacia la modernidad al incorporar plataformas digitales en el proceso de aprendizaje, ofreciendo acceso a herramientas tecnológicas y clases virtuales para todas las modalidades educativas.</div>
+                </li>
+                <li class="fourteenth_section" style="--accent-color:#969696">
+                    <div class="date">2027</div>
+                    <div class="title">Reconocimiento Regional</div>
+                    <div class="descr">El Colegio ISAN fue reconocido como una de las mejores instituciones educativas de la región, gracias a su enfoque integral en el desarrollo académico, profesional y personal de sus estudiantes.</div>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="ninth_secction">
+        <div class="ninth_section_gid">
+            <div class="ninth_section_grid_video">
+                <video controls poster="../img_sp/modelo_educativo/ninth_section.png" class="third_section_grid_himno_video">
+                    <source src="../img_sp/modelo_educativo/ninth_section.mp4" type="video/mp4">
+                    Tu navegador no soporta la reproducción de video. Por favor, actualiza tu navegador.
+                </video>
+            </div>
+            <div class="ninth_section_grid_info">
+                <h2>Profesores comprometidos por</h2>
+                <h2>el amor a la enseñanza</h2>
+                <p>El personal académico del Colegio ISAN está comprometido en ofrecer una educación que permita a cada estudiante desarrollar su potencial y convertirse en un agente de cambio en su entorno.</p>
+                <p>El profesionalismo de nuestros docentes se refleja en el éxito de cada uno de nuestros estudiantes.</p>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+</html>
---
+++ sp/preparatoria_abierta.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<link rel="stylesheet" href="../styles_sp/preescolar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+<script src="sp.js"></script>
+
+
+<body>
+
+    <section class="first_section_niveles_educativos">
+        <div class="first_section_niveles_educativos_img" style="background-image: url(../img_sp/modelo_educativo/portada.webp);"></div>
+        <div class="first_section_niveles_educativos_portada">
+            <h2 class="first_section_niveles_educativos_title">Faraday School Preschool</h2>
+            <p class="first_section_niveles_educativos_sibtitle"><strong>Prepare your little one for the future with the General Education (GE) Preschool Program.</strong></p>
+            <div class="first_section_niveles_educativos_portada_grid">
+                <div class="first_section_niveles_educativos_portada_grid_img">
+                    <img src="../img_sp/preescolar/maestro.png">
+                </div>
+                <div class="first_section_niveles_educativos_portada_grid_info">
+                    <p>Meet our <strong>principal</strong> Bibiana</p>
+                    <p>Arias Sotres of <strong>Colegio Faraday</strong></p>
+                    <a class="first_section_niveles_educativos_portada_grid_info_link" href="javascript:void(0)">Watch 1 minute video <div class="first_section_niveles_educativos_portada_grid_info_link_img"><img src="../img_sp/preescolar/play.svg"></div></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="second_section">
+        <div class="second_section_grid">
+            <div class="second_section_grid_info">
+                <h2>Schedules</h2>
+                <p>All preschool levels are taught at the Faraday School facilities.</p>
+                <ul>
+                    <li><i class="fa-solid fa-square-check"></i> <strong>Torreon Headquarters Hours:</strong> 7:30 a.m. a 3:00 p.m.</li>
+                    <li><i class="fa-solid fa-square-check"></i> <strong>Extended hours:</strong> 3:00 p.m a 4:30 pm</li>
+                </ul>
+                <h3>The maximum number of students per classroom is 20.</h3>
+            </div>
+
+            <div class="second_section_grid_img">
+                <img src="../img_sp/preescolar/second_section.png">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="third_section">
+        <div class="third_section_grid">
+            <div class="third_section_grid_info">
+                <h2>Awaken your child's potential with the International Baccalaureate Diploma (IB) PYP Program</h2>
+                <img src="../img_sp/preescolar/programa_primaria.webp">
+                <p>The PYP, or International Baccalaureate (IB) Primary School Programme, is an educational program <strong>focused on children from 3 to 12 years old</strong>, chosen by the best schools around the world.</p>
+                <p><strong>The main objective is to provide your child with a comprehensive education.</strong>, developing not only their academic knowledge, but also their social, emotional and critical thinking skills.</p>
+                <a href="javascript:void(0)"><strong>About the IB Program <i class="fa-solid fa-chevron-right"></i></strong></a>
+            </div>
+            <div class="third_section_grid_img">
+                <img src="../img_sp/preescolar/third_section.png">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="fifth_section">
+        <div class="fifth_section_flex">
+            <div class="fifth_section_flex_info">
+                <h2>Each child builds his or her learning through sensory experiences.</h2>
+                <p>Your child will reach a <strong>safe environment</strong> where he will live structured experiences according to his age, which will encourage him to investigate and build his learning.</p>
+            </div>
+            <div class="fifth_section_flex_img">
+
+                <div class="fifth_section_flex_img_div_img">
+                    <img src="../img_sp/preescolar/fifth_section_1.png">
+                    <div class="fifth_section_flex_img_div_info">
+                        <h2>Transitory 1</h2>
+                        <p>From 2 years 8 months</p>
+                    </div>
+                </div>
+
+                <div class="fifth_section_flex_img_div_arreglo">
+                    <img src="../img_sp/preescolar/arreglo.svg">
+                </div>
+
+                <div class="fifth_section_flex_img_div_img">
+                    <img src="../img_sp/preescolar/fifth_section_2.png">
+                    <div class="fifth_section_flex_img_div_info">
+                        <h2>Transitory 2</h2>
+                        <p>From 3 years 8 months</p>
+                    </div>
+                </div>
+
+                <div class="fifth_section_flex_img_div_arreglo">
+                    <img src="../img_sp/preescolar/arreglo.svg">
+                </div>
+
+                <div class="fifth_section_flex_img_div_img">
+                    <img src="../img_sp/preescolar/fifth_section_1.png">
+                    <div class="fifth_section_flex_img_div_info">
+                        <h2>Preprimary</h2>
+                        <p>From 4 years 8 months</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="sixth_section">
+        <div class="sixth_section_grid">
+            <div class="sixth_section_grid_info">
+                <p>Faraday School is the best investment for your child's future.
+                your child's future</p>
+                <h2>Tuition 2024-2025</h2>
+                <p id="second_p">Leave us your email and we will send you:</p>
+                <div class="sixth_section_grid_info_grid">
+                    <div class="sixth_section_grid_info_grid_img"><img src="../img_sp/preescolar/sixth_section_info.png"></div>
+                    <div class="sixth_section_grid_info_grid_info">
+                        <ul>
+                            <li>Enrollment for the 2024-2025 cycle</li>
+                            <li>Tuition for each educational level</li>
+                            <li>Admission Process</li>
+                            <li>Methods of payment</li>
+                            <li>Payment Schedule</li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="sixth_section_grid_info_form">
+                    <div class="sixth_section_grid_info_form_elements">
+                        <input type="email" id="email" name="email" placeholder="correo@ejemplo.com" required>
+                    </div>
+                    <div class="sixth_section_grid_info_form_elements">
+                        <input type="tel" id="telefono" name="telefono" placeholder="Número de teléfono" required>
+                    </div>
+                    <div class="sixth_section_grid_info_form_elements">
+                        <a href="javascript:void(0)" class="download_button"><strong>Download <i class="fa-solid fa-chevron-right"></i></strong></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="sixth_section_grid_img">
+                <img src="../img_sp/preescolar/sixth_section_img.png">
+            </div>
+        </div>
+    </section>
+
+    <section class="seventh_section">
+        <div class="seventh_section_grid">
+            <div class="seventh_section_img">
+                <img src="../img_sp/preescolar/seventh_section.png">
+            </div>
+            <div class="seventh_section_info">
+                <h2>At Faraday College your child will have the opportunity to practice different sports and cultural activities.</h2>
+                <div class="seventh_section_info_grid">
+                    <div class="seventh_section_info_grid_actividades_1">
+                        <ul>
+                            <li>Dance</li>
+                            <li>Yoga</li>
+                            <li>Soccer</li>
+                            <li>Art</li>
+                            <li>Psychomotor</li>
+                        </ul>
+                    </div>
+                    <div class="seventh_section_info_grid_actividades_1">
+                        <ul>
+                            <li>Taekwondo</li>
+                            <li>Feelings and emotions</li>
+                            <li>Ballet</li>
+                            <li>Gymnastics</li>
+                            <li>Creative imagination</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="eighth_section">
+        <div class="eighth_section_grid">
+            <div class="eighth_section_info">
+                <h2>Educational farm</h2>
+                <p>Faraday College offers <strong>a place for your child to learn about respect for the environment</strong>, as well as love and care for animals.</p>
+                <p>From preschool through high school, our students become aware of their role as agents of change, and share it with their family and friends..</p>
+            </div>
+            <div class="eighth_section_img">
+                <img src="../img_sp/preescolar/eighth_section.webp">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="ninth_section">
+        <div class="ninth_section_title">
+            <h2>Each space is designed for them to learn<br>
+            in a safe and fun environment</h2>
+        </div>
+        <div class="ninth_section_info">
+            <h2>Faraday College</h2>
+            <p>Take a virtual tour of our facilities</p>
+            <a href="javascript:void(0)"><strong>Perform tour <i class="fa-solid fa-chevron-right"></i></strong></a>
+        </div>
+    </section>
+
+
+    <section class="tenth_section">
+        <div class="tenth_section_grid">
+            <div class="tenth_section_grid_info">
+                <h2>English language teaching journey begins</h2>
+                <p>Children have the ability to learn a new language naturally, so we teach English through oral, written and visual communication.</p>
+                <p><strong>Your little one will take fully bilingual classes: 50% of the day in English and 50% in Spanish..</strong></p>
+            </div>
+            <div class="tenth_section_grid_video">
+                <img src="../img_sp/preescolar/tenth_section.png">
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="eleventh_section">
+        <div class="eleventh_section_grid">
+            <div class="eleventh_section_grid_video">
+                <img src="../img_sp/preescolar/eleventh_section.png">
+            </div>
+            <div class="eleventh_section_grid_info">
+                <h2>Psychomotor skills program</h2>
+                <p>Our priority is for your child to develop sensory, affective, emotional and socialization skills.</p>
+                <p>Through psychomotor activities, it works with motor coordination, group integration and body image and expression.</p>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="twelfth_section">
+        <div class="twelfth_section_grid">
+            <div class="twelfth_section_grid_div" id="twelfth_section_grid_div_id1">
+                <h2>Faraday Blog</h2>
+                <p>News about school activities, <strong>articles on educational trends and tips</strong> for every stage in your children's lives.</p>
+            </div>
+
+            <div class="twelfth_section_grid_div twelfth_section_second_div" id="twelfth_section_grid_div_id2">
+                <div class="twelfth_section_second_div_img">
+                    <img src="../img_sp/becas_convenios/seventh_section_div2.webp">
+                </div>
+                <div>
+                    <h3>High schools with scholarships to the best universities</h3>
+                    <p>Having a successful professional career is part of the results of studying at the leading universities in ...</p>
+                </div>
+                <div class="twelfth_section_second_div_button">
+                    <a href="javascript:void(0)"><strong>Read more <i class="fa-solid fa-arrow-right"></i></strong></a>
+                </div>
+            </div>
+
+            <div class="twelfth_section_grid_div twelfth_section_second_div" id="twelfth_section_grid_div_id3">
+                <div class="twelfth_section_second_div_img">
+                    <img src="../img_sp/becas_convenios/seventh_section_div2.webp">
+                </div>
+                <div>
+                    <h3>International Baccalaureate IB</h3>
+                    <p>Since the International Baccalaureate was implemented in several private high schools worldwide, it has achieved ...</p>
+                </div>
+                <div class="twelfth_section_second_div_button">
+                    <a href="javascript:void(0)"><strong>Read more <i class="fa-solid fa-arrow-right"></i></strong></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
